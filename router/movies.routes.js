aac6c77ebeb49678a9f52fa0f7dfdf725e968026
Azzahid Poeloengan <faiz.poeloengan@gmail.com>
2022-02-20 01:27:12 +0700

    refactor: modify folder name

--- router/movies.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.get('/movies', getAllMovies);
-router.get('/movies/:title', getMovie);
-router.get('/movies/favorite', getFavorites);
-router.post('/movies/favorite', addFavorites);
-
-module.exports = router;
