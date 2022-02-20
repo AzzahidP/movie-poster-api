187b0cac9555f0fffae8016714903e0b3e241a2e
Azzahid Poeloengan <faiz.poeloengan@gmail.com>
2022-02-20 13:00:27 +0700

    add favorites controllers

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const axios = require('axios');
+const {Favorite_Movies} = require('../models')
 
 const getAllMovies = (req, res) => {
     try {
@@ -34,4 +35,56 @@ const getOneMovie = async (req, res) => {
           });
 }
 
-module.exports = {getAllMovies, getOneMovie}
+const getFavorites = (req, res) => {
+
+    userId = req.body.user_id
+
+    Favorite_Movies.findAll({
+        where: {
+            user_id: userId
+        }
+    })
+    .then((movies) => {
+        res.status(200).json({
+            result: "SUCCESS",
+            message: movies
+        })
+    })
+    .catch((err) => {
+        res.status(500).json({
+            result: "FAILED",
+            message: err.message || 'Internal server error'
+        })
+    });
+};
+
+const addFavorites = (req, res) => {
+
+    if (!req.body.title || !req.body.user_id){
+        res.status(400).json({
+            result: "FAILED",
+            message: "Please provide Mvoie Title and User ID"
+        });
+        return;
+    }    
+
+    const newFav = {
+        title: req.body.title,
+        user_id: req.body.user_id
+    }
+    Favorite_Movies.create(newFav)
+        .then( () => {
+            res.status(201).json({
+                result: "SUCCESS",
+                message: `Added ${req.body.title} as ${req.body.user_id}'s favorite`
+            })
+        })
+        .catch((err) => {
+            res.status(500).json({
+                result: "FAILED",
+                message: err.message || "Internal server error"
+            })
+        })
+}
+
+module.exports = {getAllMovies, getOneMovie, getFavorites, addFavorites}
