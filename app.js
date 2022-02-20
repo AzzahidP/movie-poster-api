0f14cd1371b216e668a93b195fa956c4daa1bdf8
Azzahid Poeloengan <faiz.poeloengan@gmail.com>
2022-02-20 19:00:15 +0700

    add body parser

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const {logger, expressLogger} = require('./middleware/logger');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const Router = require('./routes/movies.routes');
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 app.use(cors());
 app.use(expressLogger);
